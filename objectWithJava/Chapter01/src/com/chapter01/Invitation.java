974edac33bff1ef08c4d43cd1352588ba07deea1
k1oo <kbaek1202@gmail.com>
2021-10-27 00:26:03 +0900

    [JAVA] Chapter01-01: 티켓 판매 애플리케이션 구현

---
+++ objectWithJava/Chapter01/src/com/chapter01/Invitation.java
@@ -0,0 +1,7 @@
+package com.chapter01;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+  private LocalDateTime when;
+}
---
+++ objectWithJava/Chapter01/src/com/chapter01/Ticket.java
@@ -0,0 +1,9 @@
+package com.chapter01;
+
+public class Ticket {
+  private Long fee;
+
+  public Long getFee() {
+    return fee;
+  }
+}
