be25c9e54a90ba0db2502c99075f5e30ebeaea1e
k1oo <kbaek1202@gmail.com>
2021-11-02 22:21:15 +0900

    [Java] TicketSeller 클래스 내부 불필요한 의존성 제거

--- objectWithJava/src/main/com/chapter01/TicketSeller.java
+++ objectWithJava/src/main/com/chapter01/TicketSeller.java
@@ -8,6 +8,6 @@ public class TicketSeller {
   }
   
   public void sellTo(Audience audience) {
-    ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
+    this.ticketOffice.sellTicketTo(audience);
   }
 }
